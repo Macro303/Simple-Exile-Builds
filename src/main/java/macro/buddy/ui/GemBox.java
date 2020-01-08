78fc38b340d78f77391afc9339ec3d3e3815fc35
Macro303 <Macro303@users.noreply.github.com>
2020-01-08 17:17:31 +1300

    Update font sizing

--- src/main/java/macro/buddy/ui/GemBox.java
+++ src/main/java/macro/buddy/ui/GemBox.java
@@ -6,10 +6,13 @@ import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import javafx.scene.text.Font;
+import javafx.util.Duration;
 import macro.buddy.Util;
 import macro.buddy.builds.BuildGem;
 import macro.buddy.builds.BuildInfo;
@@ -40,23 +43,23 @@ public class GemBox extends GridPane {
 		setPadding(new Insets(5.0));
 		setStyle(getBorderStyle());
 
-		ImageView gemImage = getGemImage();
+		var gemImage = getGemImage();
 		add(gemImage, 0, 0, 3, 1);
 		setHalignment(gemImage, HPos.CENTER);
 
-		Label nameLabel = getNameLabel();
+		var nameLabel = getNameLabel();
 		add(nameLabel, 0, 1, 3, 1);
 		setVgrow(nameLabel, Priority.ALWAYS);
 
-		Button previousButton = getPreviousButton();
+		var previousButton = getPreviousButton();
 		if (previousButton.isVisible())
 			add(previousButton, 0, 2);
 
-		Button nextButton = getNextButton();
+		var nextButton = getNextButton();
 		if (nextButton.isVisible())
 			add(nextButton, 2, 2);
 
-		Separator separator = new Separator(Orientation.HORIZONTAL);
+		var separator = new Separator(Orientation.HORIZONTAL);
 		separator.setVisible(false);
 		setHgrow(separator, Priority.ALWAYS);
 		if (previousButton.isVisible() || nextButton.isVisible())
@@ -68,60 +71,76 @@ public class GemBox extends GridPane {
 	}
 
 	public ImageView getGemImage() {
-		String imageFile = getClass().getResource("placeholder[80x80].png").toExternalForm();
+		var imageFile = getClass().getResource("placeholder[80x80].png").toExternalForm();
 		if (gem.isPresent()) {
-			String temp = String.format("gems\\%s", gem.get().getFilename());
+			var temp = String.format("gems\\%s", gem.get().getFilename());
 			if (new File(temp).exists())
 				imageFile = "file:" + temp;
 		}
-		ImageView image = new ImageView(new Image(imageFile));
+		var image = new ImageView(new Image(imageFile));
 		image.setFitHeight(80);
 		image.setFitWidth(80);
 		return image;
 	}
 
 	public Label getNameLabel() {
-		Label nameLabel = new Label(gem.isPresent() ? gem.get().getInfo().getName() : "Missing Gem");
+		var nameLabel = new Label(gem.isPresent() ? gem.get().getInfo().getName() : "Missing Gem");
 		nameLabel.setWrapText(true);
 		nameLabel.setPrefWidth(90);
 		return nameLabel;
 	}
 
 	public Button getPreviousButton() {
-		Button previous = new Button("<<");
-		var previousGem = build.getUpdates().stream().filter(update -> {
+		var previousButton = new Button("<<");
+		var previousUpdate = build.getUpdates().stream().filter(update -> {
 			var newGem = BuildUtils.getGem(update.getNewGem());
 			if (newGem.isEmpty())
 				return false;
 			return newGem.get().getFullname().equalsIgnoreCase(gem.isPresent() ? gem.get().getFullname() : "Missing Gem");
-		}).map(update -> BuildUtils.getGem(update.getOldGem())).flatMap(Optional::stream).findFirst();
-		previous.setVisible(previousGem.isPresent());
-		previous.setOnAction(event -> {
+		}).findFirst();
+		previousButton.setVisible(previousUpdate.isPresent());
+		if (previousUpdate.isEmpty())
+			return previousButton;
+		var previousGem = BuildUtils.getGem(previousUpdate.get().getOldGem());
+		if (previousGem.isEmpty())
+			LOGGER.warn("Missing Gem Entry for {}", previousUpdate.get().getOldGem());
+		previousButton.setOnAction(event -> {
 			if (previousGem.isPresent()) {
 				LOGGER.info("Previous Selected, Updating {} to {}", gem.isPresent() ? gem.get().getFullname() : "Missing Gem", previousGem.get().getFullname());
 				gem = previousGem;
 				initialize();
 			}
 		});
-		return previous;
+		return previousButton;
 	}
 
 	public Button getNextButton() {
-		Button next = new Button(">>");
-		var nextGem = build.getUpdates().stream().filter(update -> {
+		var nextButton = new Button(">>");
+		var nextUpdate = build.getUpdates().stream().filter(update -> {
 			var oldGem = BuildUtils.getGem(update.getOldGem());
 			if (oldGem.isEmpty())
 				return false;
 			return oldGem.get().getFullname().equalsIgnoreCase(gem.isPresent() ? gem.get().getFullname() : "Missing Gem");
-		}).map(update -> BuildUtils.getGem(update.getNewGem())).flatMap(Optional::stream).findFirst();
-		next.setVisible(nextGem.isPresent());
-		next.setOnAction(event -> {
+		}).findFirst();
+		nextButton.setVisible(nextUpdate.isPresent());
+		if (nextUpdate.isEmpty())
+			return nextButton;
+		var nextGem = BuildUtils.getGem(nextUpdate.get().getNewGem());
+		if (nextGem.isEmpty())
+			LOGGER.warn("Missing Gem Entry for {}", nextUpdate.get().getNewGem());
+		nextButton.setOnAction(event -> {
 			if (nextGem.isPresent()) {
 				LOGGER.info("Next Selected, Updating {} to {}", gem.isPresent() ? gem.get().getFullname() : "Missing Gem", nextGem.get().getFullname());
 				gem = nextGem;
 				initialize();
 			}
 		});
-		return next;
+		if (nextButton.isVisible()){
+			var tooltip = new Tooltip(nextUpdate.get().getReason());
+			tooltip.setShowDelay(Duration.seconds(0));
+			tooltip.setStyle("-fx-font-size: 10pt;");
+			nextButton.setTooltip(tooltip);
+		}
+		return nextButton;
 	}
 }
